2f7145546b012eff3dbb25bf44ac26d909d691a1
sarthakportal2 <77792446+sarthakportal2@users.noreply.github.com>
2024-04-08 20:30:56 +0530

    Nim_Game.py

--- Nim_Game.py
+++ Nim_Game.py
@@ -1,4 +1,5 @@
 class Solution:
     def canWinNim(self, n: int) -> bool:
-        if n%4==0:return false
-        else return true
+        #T(C(N)==O(1)) and S(C(N)=O(1)) as it requires non additional Space in Unit time 
+        if n%4==0: return False#printing false to the UNoptimal stone's heeap
+        else: return True#Pritning True to FIrst Stone heap

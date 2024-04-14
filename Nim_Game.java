3a6228a1e6313936d89097ef45f6790648a69a94
sarthakportal2 <77792446+sarthakportal2@users.noreply.github.com>
2024-04-14 22:36:30 +0530

    Nim_Game.java

---
+++ Nim_Game.java
@@ -0,0 +1,5 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        //T(C(N)=O(1)) and S(C(N)=O(1)) as it requires not addiitonal space with in unit Time        
+        if (n%4==0)return false;//Print false to the UnOptimal heap Play
+        else return true;}}//Printing True to The Optimal Play Soln
